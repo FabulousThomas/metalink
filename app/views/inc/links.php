62e5725b60baae6ffc60e6028c4a5d6cb17102ad
Mba Thomas <angelfab631@gmail.com>
2023-06-22 07:50:29 +0200

    Create links.php

---
+++ app/views/inc/links.php
@@ -0,0 +1,15 @@
+<!-- FAVICON -->
+<link rel="shortcut icon" href="<?= URLROOT ?>/assets/images/favicon.svg" type="image/svg+xml">
+<!-- FONT AWESOME -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+   integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+   crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- LINE AWESOME -->
+<link rel="stylesheet"
+   href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+<!-- GOOGLE FONTS LINK -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap" rel="stylesheet">
+<!-- BOOTSTRAP CSS LINK -->
+<link rel="stylesheet" href="<?= URLROOT ?>/assets/css/bootstrap-5.2.3.min.css">
