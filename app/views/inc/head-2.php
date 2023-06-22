0ab50c7a387019ca2727475c6ee5d9d64f76d824
Mba Thomas <angelfab631@gmail.com>
2023-06-22 07:50:13 +0200

    Create head-2.php

---
+++ app/views/inc/head-2.php
@@ -0,0 +1,26 @@
+<?php include "./directories.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <!-- FONTAWESOME -->
+    <!-- <script src="https://kit.fontawesome.com/34a3f5726e.js" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" />
+
+    <!-- bootstrap css link -->
+    <link rel="stylesheet" href="<?= URLROOT ?>/assets/css/bootstrap-5.2.3.min.css">
+
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/assets/css/style-2.css" />
+
+    <script src="<?= URLROOT; ?>/assets/js/jquery-3.6.1.min.js"></script>
+    <script src="<?= URLROOT; ?>/assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+    <title>Metalink | Admin</title>
+</head>
+
+<body>
