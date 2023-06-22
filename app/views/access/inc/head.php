224f29360ae617ba778b940e6ffb07e3a43c16d7
Mba Thomas <angelfab631@gmail.com>
2023-06-22 07:48:12 +0200

    Create head.php

---
+++ app/views/access/inc/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- META TAGS -->
+    <?php include APPROOT . "/views/inc/metas.php"; ?>
+    <!-- LINKS -->
+    <?php include APPROOT . "/views/inc/links.php"; ?>
+    <!-- CUSTOME CSS -->
+    <link rel="stylesheet" href="<?= URLROOT; ?>/assets/css/style-2.css" />
+    <!-- SCRIPTS -->
+    <script src="<?= URLROOT; ?>/assets/js/jquery-3.6.1.min.js"></script>
+    <script src="<?= URLROOT; ?>/assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+    <title>Metalink | Admin</title>
+</head>
+
+<body>
