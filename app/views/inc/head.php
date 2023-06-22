2cd6f06596035359e2c1d2c9689f0606707016e6
Mba Thomas <angelfab631@gmail.com>
2023-06-22 07:50:17 +0200

    Create head.php

---
+++ app/views/inc/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <!-- META TAGS -->
+   <?php include APPROOT . "/views/inc/metas.php"; ?>
+   <!-- LINKS -->
+   <?php include APPROOT . "/views/inc/links.php"; ?>
+   <!-- CUSTOME CSS -->
+   <link rel="stylesheet" href="<?= URLROOT; ?>/assets/css/style.css">
+   <title>Metalink - Discover rate collection or Arts & NFTs</title>
+
+</head>
+
+<body id="top">
