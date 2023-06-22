e95b413d5c7c02015aab600b3fb8b6c03549319a
Mba Thomas <angelfab631@gmail.com>
2023-06-22 07:50:07 +0200

    Create footer.php

---
+++ app/views/inc/footer.php
@@ -0,0 +1,207 @@
+<footer class="footer">
+
+   <div class="section footer-top">
+      <div class="container">
+
+         <div class="footer-brand">
+
+            <?= SITELOGO_LG ?>
+
+            <p class="body-md footer-text">
+               Buy, sell and discover exclusive digital assets by the top artists of NFTs world.
+            </p>
+
+         </div>
+
+         <ul class="footer-list">
+
+            <li>
+               <p class="title-lg footer-list-title">Metalink</p>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Explore Item</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Live Auction</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Activities</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Wallets</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Creators</span>
+               </a>
+            </li>
+
+         </ul>
+
+         <ul class="footer-list">
+
+            <li>
+               <p class="title-lg footer-list-title">Useful Links</p>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">About Us</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Blog & News</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Terms & Condition</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Privacy policy</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Login</span>
+               </a>
+            </li>
+
+            <li>
+               <a href="#" class="footer-link">
+                  <ion-icon name="chevron-forward"></ion-icon>
+
+                  <span class="span">Contact Us</span>
+               </a>
+            </li>
+
+         </ul>
+
+         <div class="footer-list">
+
+            <ul>
+
+               <li>
+                  <p class="title-lg footer-list-title">Download the Metalink app</p>
+               </li>
+
+               <li class="footer-list-item">
+                  <a href="#">
+                     <img src="./assets/images/appstore.png" width="134" height="40" loading="lazy"
+                        alt="Download Metalink app from AppStore">
+                  </a>
+
+                  <a href="#">
+                     <img src="./assets/images/playstore.png" width="134" height="40" loading="lazy"
+                        alt="Download Metalink app from PlayStore">
+                  </a>
+               </li>
+
+            </ul>
+
+            <ul>
+
+               <li>
+                  <p class="title-lg footer-list-title">Contact Details</p>
+               </li>
+
+               <li class="footer-list-item">
+                  <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
+
+                  <a href="mailto:metalink@nft.com" class="footer-link">metalink@nft.com</a>
+               </li>
+
+               <li class="footer-list-item">
+                  <ion-icon name="call-outline" aria-hidden="true"></ion-icon>
+
+                  <a href="tel:+111 234-567-891" class="footer-link">+111 234-567-891</a>
+               </li>
+
+            </ul>
+
+         </div>
+
+      </div>
+   </div>
+
+   <div class="footer-bottom">
+      <div class="container">
+
+         <p class="body-md text-center copyright">
+            &copy;
+            <script>
+               document.write(new Date().getFullYear())
+            </script> Metalink. | All rights reserved
+         </p>
+
+      </div>
+   </div>
+
+   <?php require "modal.php"; ?>
+
+</footer>
+
+
+<!-- #BACK TO TOP -->
+<a href="#top" class="back-to-top btn-icon" aria-label="back to top" data-back-top-btn>
+   <ion-icon name="arrow-up" aria-hidden="true"></ion-icon>
+</a>
+
+
+<div class="body-bg-shape"></div>
+
+
+<script src="<?= URLROOT; ?>/assets/js/jquery-3.6.1.min.js"></script>
+<script src="<?= URLROOT; ?>/assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+
+<!-- custom js link -->
+<script src="<?= URLROOT; ?>/assets/js/script.js"></script>
+<script src="<?= URLROOT ?>/assets/js/tab.js"></script>
+
+<!-- ionicon link -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+
+</html>
