738cf51c7ad0fb65ea6b440cfbf249cd10b4bbe9
Mba Thomas <angelfab631@gmail.com>
2023-06-22 07:50:32 +0200

    Create metas.php

---
+++ app/views/inc/metas.php
@@ -0,0 +1,7 @@
+<!-- PRIMARY META TAGS -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<meta name="title" content="Metalink - Discover rate collection or Arts & NFTs">
+<meta name="description"
+   content="We are a huge marketplace dedicated to connecting great artists of all Metalink with their fans and unique token collectors!">
