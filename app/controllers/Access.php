2712e9bd6ae3e3fac9f4b1f2a4e0d1781196705d
Mba Thomas <angelfab631@gmail.com>
2023-06-26 02:30:59 +0200

    Update Access.php

--- app/controllers/Access.php
+++ app/controllers/Access.php
@@ -1,10 +1,8 @@
 <?php
-
 class Access extends Controller
 {
-
    private $data = [];
-   private $adminModel = '';
+   private $pageModel = '';
 
    public function __construct()
    {
@@ -12,31 +10,41 @@ class Access extends Controller
          unset($_SESSION['isLoggedIn']);
          $this->view('access/index');
       }
+      $this->pageModel = $this->model('Page');
    }
 
    public function index()
    {
-      if (isset($_SESSION['user_id'])) {
-         global $conn;
+      if (isLoggedin()) {
          $u_id = $_SESSION['user_id'];
-         $u_access = $_SESSION['user_access'];
-         $result = $conn->query("SELECT * FROM users WHERE user_id = '$u_id' AND user_access = '1'");
+
+         $singleResult = $this->pageModel->selectWhereAnd('users', 'user_id', $u_id, 'user_access', '1');
+         $result = $this->pageModel->getAll('nfts', 'nft_id');
+
+         $this->data = [
+            'results' => $result,
+            'singleResult' => $singleResult,
+         ];
       }
 
-      // if (isLoggedin()) {
-         $row = mysqli_fetch_assoc($result);
-         if (isset($row) && $row['user_access'] === $u_access) {
-            $this->data = [
-               'title' => 'ADMIN PANEL',
-               'user_bio' => $row['user_bio'],
-               'user_name' => $row['user_name'],
-               'user_fname' => $row['user_fname'],
-               'user_image' => $row['user_image'],
-               'user_access' => $u_access,
-            ];
-         }
-      // }
-
       $this->view('/access/index', $this->data);
+   }
+
+
+   public function orders()
+   {
+      if (isLoggedin()) {
+         $u_id = $_SESSION['user_id'];
+
+         $singleResult = $this->pageModel->selectWhereAnd('users', 'user_id', $u_id, 'user_access', '1');
+         $result = $this->pageModel->getAll('nfts', 'nft_id');
+
+         $this->data = [
+            'results' => $result,
+            'singleResult' => $singleResult,
+         ];
+      }
+
+      $this->view('/access/orders', $this->data);
    }
 }
