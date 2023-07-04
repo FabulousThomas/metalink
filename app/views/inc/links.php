8ed93ce0d430b1b019ab51c823edc1f85e881520
Mba Thomas <angelfab631@gmail.com>
2023-07-04 18:22:18 -0500

    Update links.php

--- app/views/inc/links.php
+++ app/views/inc/links.php
@@ -13,3 +13,10 @@
 <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap" rel="stylesheet">
 <!-- BOOTSTRAP CSS LINK -->
 <link rel="stylesheet" href="<?= URLROOT ?>/assets/css/bootstrap-5.2.3.min.css">
+
+<link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/colreorder/1.6.2/css/colReorder.dataTables.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/rowreorder/1.3.3/css/rowReorder.dataTables.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/scroller/2.1.1/css/scroller.dataTables.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/searchpanes/2.1.2/css/searchPanes.dataTables.min.css" rel="stylesheet"/>
